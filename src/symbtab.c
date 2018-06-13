ce9e59e190778afc26b39ffb347d9403d4644562
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2018-06-13 17:27:38 -0400

    Wrote symbol table in preparation of parser (+ imported UTHash header)

---
+++ src/symbtab.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>  /* malloc */
+#include <string.h>  /* strdup */
+#include "uthash.h"
+
+#include "symbtab.h"
+
+/*struct _nameid_pair {
+    const char *name;
+    int         id;
+
+    UT_hash_handle hh;
+};*/
+
+int _NEXT_ID = -1;
+int _MAX_TAB = 64;
+struct _nameid_pair *symbols_by_name = NULL;
+char **symbols_by_id = NULL;
+
+int symb_lookup(char *name) {
+    struct _nameid_pair *record;
+
+    HASH_FIND_STR( symbols_by_name, name, record );
+
+    // Existing symbol
+    if (record) {
+        return record->id;
+    }
+    
+    // New symbol
+    if (_NEXT_ID == -1) {
+        symbols_by_id = (char **)malloc(sizeof(char *) * _MAX_TAB);
+        _NEXT_ID = 0;
+    } else if (_NEXT_ID >= _MAX_TAB) {
+        _MAX_TAB = 2*_MAX_TAB;
+        symbols_by_id = (char **)realloc(symbols_by_id, sizeof(char *) * _MAX_TAB);
+    }
+    int newid = _NEXT_ID++;
+    record = (struct _nameid_pair *)malloc(sizeof *record);
+    record->name = strdup(name);
+    record->id = newid;
+
+    HASH_ADD_KEYPTR( hh, symbols_by_name, record->name, strlen(record->name), record );
+    symbols_by_id[record->id] = record->name;
+
+    return record->id;
+}
+
+char *symb_lookup_by_id(int id) {
+    // Check if id is out of bounds.
+    if (id < 0 || id >= _MAX_TAB || id >= _NEXT_ID) {
+        return NULL;
+    }
+
+    return symbols_by_id[id];
+}
---
+++ src/symbtab.h
@@ -0,0 +1,25 @@
+#ifndef _SYMBTAB_H
+#define _SYMBTAB_H
+
+// Macros for a fast hashmap.
+#include "uthash.h"
+
+// Key-value pair prototype.
+struct _nameid_pair {
+    const char *name;
+    int         id;
+
+    UT_hash_handle hh;
+};
+
+// These fields must be external so a global state can be stored.
+extern int _NEXT_ID;
+extern int _MAX_TAB;
+extern struct _nameid_pair *symbols_by_name;
+extern char **symbols_by_id;
+
+// Core methods for alpha-renaming of parameters.
+int symb_lookup(char *name);
+char *symb_lookup_by_id(int id);
+
+#endif
