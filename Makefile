314b22c5bf56ca81b380745f8e3e24be5ff5d683
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2018-06-14 19:31:13 -0400

    Added standalone oracle program, using combstruct2json integration.

--- Makefile
+++ Makefile
@@ -1,3 +1,11 @@
+
+boltzoc: src/*.c libcombstruct2json.a combstruct2json.h
+	gcc -o boltzoc -Wno-unused-value \
+		-L. -I/opt/local/include -lcombstruct2json \
+		src/oracle.c \
+		src/symbtab.c src/system.c src/iter.c src/jacob.c src/diff.c \
+		src/eval.c src/print.c src/equation.c src/common.c src/parse.c
+
 all:
 	make -C src
 	make -C test
---
+++ src/oracle.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "combstruct.h"
+#include "parse.h"
+
+int main(int argc, char **argv)
+{
+
+  char *filename;
+
+  // Process command line parameters.
+  if (argc > 1) {
+    filename = argv[1];
+  } else {
+    fprintf(stderr, "// Warning: No input file specified, defaulting to /dev/stdin\n");
+    filename = "/dev/stdin";
+  }
+
+  // Parse grammar, save symbol names, and run oracle.
+  char **symbols = NULL;
+  comb_sys s = importGrammar(filename, &symbols);
+  double z = sing_sys(s, 10e-12, 10e-5);
+
+  // Output singularity
+  printf("// === SINGULARITY ===\n");
+  printf("#define %-20s %.20lf\n", "Z", z);
+  for(int i=0; symbols[i] != NULL; i++) {
+    printf("#define %-20s %.10lf\n", symbols[i], sys_val(s, i+1));
+  }
+
+  free_sys(s);
+  return 0;
+}
